9e59622ff5d578260af03545f000e782cc951e72
hmimran031 <hmimran031@hmail.com>
2024-10-06 23:19:07 +0600

    final commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// Number 1
+let saraApples = 25;
+let boughtApples = 15;
+let totalApples = saraApples + boughtApples;
+console.log("Total apples Sara has:", totalApples);
+
+// Number 2
+let joneMoney = 500;
+let spentMoney = 200;
+let remainingMoney = joneMoney - spentMoney;
+console.log("Money left with Jone:", remainingMoney);
+
+// Number 3
+let machinesPerHour = 120;
+let hoursWorked = 8;
+let totalMachines = machinesPerHour * hoursWorked;
+console.log("Total machines produced:", totalMachines);
+
+// Number 4
+let treesPerRow = 12;
+let totalRows = 8;
+let currentTrees = treesPerRow * totalRows;
+
+let newTrees = 20;
+let totalTrees = currentTrees + newTrees;
+
+console.log("total tree number: ", totalTrees);
+
+// Number 5
+let rahimBox = 5;
+let perBoxorange = 10;
+let totalOrange = rahimBox * perBoxorange;
+
+let givenOranges = 12;
+let remainingOranges = totalOrange - givenOranges;
+console.log("Remaining oranges with Rahim:", remainingOranges);
+
+// Number 6
+let totalDistance = 150;
+let totalTime = 3;
+let speedPerHour = totalDistance / totalTime;
+console.log("Speed of the train per hour:", speedPerHour, "km/h");
+
+// Number 7
+let machines = 6;
+let productionPerDay = 200;
+
+let productionPerMachine = productionPerDay / machines;
+
+let newMachines = 8;
+let newProduction = productionPerMachine * newMachines;
+
+let unsoldProducts = 3;
+let remainingProducts = newProduction - unsoldProducts;
+
+console.log("Total production on 8 machines: " + newProduction);
+console.log(
+  "Remaining products if 3 products are not sold: " + remainingProducts
+);
+
+// Number 8
+let totalChocklet = 120;
+let totalPackets = 9;
+let totalremainingChocklet = totalChocklet % totalPackets;
+console.log("Remaining Chocklet after packing", totalremainingChocklet);
